9882ae0ba83de011beb025367ac26401cf21e945
Griffin Tang <gltang@andrew.cmu.edu>
2021-01-06 20:47:06 -0800

    Messages and turtles should be ready

--- Message.lua
+++ Message.lua
@@ -1,12 +1,12 @@
 -- suck my toes krab
 os.loadAPI(tools4fools)
-Message = {time = getTime(),
-		sender = os.getComputerID(),
-		y = 0,
-		z = 0,
-		fuel = 0,
-		dimension = 'earth',
-		task = nil
+Message = { time = getTime(),
+			sender = nil,
+			reciever = '',
+			message = '',
+			read = false,
+			dimension = 'earth',
+			task = nil
 	}
 
 function Message:create(o)
--- trtl.lua
+++ trtl.lua
@@ -1,5 +1,6 @@
 -- suck my toes krab
 os.loadAPI(tools4fools)
+os.loadAPI(Message)
 
 Trtl = {id = os.getComputerid(),
 		x = 0,
@@ -7,7 +8,7 @@ Trtl = {id = os.getComputerid(),
 		z = 0,
 		fuel = 0,
 		dimension = 'earth',
-		time = tools.getTime()
+		time = getTime(),
 		task = nil
 	}
 
@@ -19,3 +20,9 @@ end
 function Trtl:checkTime() --Return the turtle calculated time (custom unix time from epoc of minecraft world)
   self.time = getTime()
 end
+
+function Trtl:sendMessage(recipient,text)
+	--need to open rednet?
+	rednet.open("left")
+	msg = Message:create(sender = self, reciever = recipient, message = text)
+	rednet.send(recipient,msg)
