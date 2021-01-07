70bc50042b9a00a13d68830191139df02328f94f
Griffin Tang <gltang@andrew.cmu.edu>
2021-01-06 21:10:17 -0800

    learn how to code bitch please

--- MasterComputerInbox.lua
+++ MasterComputerInbox.lua
@@ -3,17 +3,19 @@ os.loadAPI("tools4fools")
 MasterComp = {}
 
 function MasterComp:receiveAndWrite()
-	while true do
-		id, msg = rednet.receive("server")
-		filename = msg.sender.id .. "_to_" .. msg.receiver .. tools4fools.getTime()
-		path = "/disk/" .. msg.receiver .. "/" .. filename
-		out = fs.open(path, "w")
-		out.write(textutils.serialize())
-		out.close()
+	id, msg = rednet.receive("server")
+	filename = msg.sender.id .. "_to_" .. msg.receiver .. tools4fools.getTime()
+	path = "/disk/" .. msg.receiver .. "/" .. filename
+	out = fs.open(path, "w")
+	out.write(textutils.serialize())
+	out.close()
 end
 
 function MasterComp:main()
-	self.receiveAndWrite()
+	rednet.open("left")
+	while true do
+		self.receiveAndWrite()
+	end
 end
 
 
