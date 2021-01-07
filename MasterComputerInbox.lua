5aaf921b8e1ddef0ed5ecf573563c3e1fd93fb03
Kyle Slager <kwslager@gmail.com>
2021-01-06 22:37:42 -0600

    Update MasterComputerInbox.lua

--- MasterComputerInbox.lua
+++ MasterComputerInbox.lua
@@ -2,14 +2,17 @@
 os.loadAPI("tools4fools")
 MasterComp = {}
 
-function MasterComp:main()
+function MasterComp:receiveAndWrite()
 	while true
 		id, msg = rednet.receive("server")
 		filename = msg.sender .. "_to_" .. msg.receiver .. tools4fools.getTime()
 		path = "/disk/" .. msg.receiver .. "/" .. filename
 		out = fs.open(path, "w")
 		out.write(textutils.serialize())
-		out.close() 
+		out.close()
+
+function MasterComp:main()
+	self.receiveAndWrite()
 
 
 
