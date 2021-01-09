8d513b673345a715e931595c4729bda7395427c1
Kyle Slager <kwslager@gmail.com>
2021-01-08 20:56:03 -0600

    butt f*cked paul bunyan
    i put it in his *ss. johnny appleseed too took it real deep. god how he moaned

--- trtl.lua
+++ trtl.lua
@@ -96,6 +96,89 @@ function Trtl:runMiningSequence(laps)
 end
 
 
+--plz enjoy this code, it makes a line of trees and chops them
+function Trtl:runLumberjackMeOff(numtrees)
+	print("fuel goes in slot 1")
+	print("Please make sure there is dirt in slot 2, saplings in slot 3, and the respective log in slot 4")
+	
+	--@XxGRIFFASCOPESxXadd your fueling code in here plz
+
+	turtle.up()
+	turtle.turnLeft()
+	turtle.turnLeft()
+	turtle.back()
+	turtle.back()
+	turtle.back()
+
+	--tree planting
+	for i = 0, numtrees do
+  		turtle.digDown()
+  		turtle.select(2)
+  		turtle.placeDown()
+  		turtle.back()
+  		turtle.select(3)
+  		turtle.place()
+  		turtle.back()
+  		turtle.back()
+  		turtle.back()
+	end
+	turtle.turnLeft()
+	turtle.forward()
+	turtle.turnRight()
+
+	--tree chopping
+	while true do
+  		os.sleep(30)
+  		for i = 0, numtrees do
+    		turtle.forward()
+    		turtle.forward()
+    		turtle.forward()
+    		turtle.forward()
+    		turtle.turnRight()
+    		turtle.select(4)
+    		-- if the tree has grown
+    		if turtle.compare() then
+      			turtle.dig()
+      			turtle.forward()
+      			-- harvest the tree
+      			while turtle.detectUp() do
+      				turtle.digUp()  
+      				turtle.up()
+      			end
+      			-- return to the ground
+      			while not turtle.detectDown() do
+       				turtle.down()
+      			end
+      			-- plant a new sapling
+      			turtle.back()
+      			turtle.select(3)
+      			turtle.place()
+    		end
+    		turtle.turnLeft()
+  		end
+  		-- round the corner
+  		turtle.forward()
+  		turtle.turnRight()
+  		turtle.forward()
+  		if turtle.getItemCount(5) == 64 then
+  			turtle.turnLeft()
+  			turtle.forward()
+  			turtle.forward()
+  			turtle.down()
+  			turtle.select(5)
+  			turtle.drop()
+  			turtle.up()
+  			turtle.back()
+  			turtle.back()
+  			turtle.turnRight()
+  		end
+  		turtle.forward()
+  		turtle.turnRight()
+  		turtle.forward()
+	end
+end
+
+
 
 
 
