ff0e1a7aa2e691728c1f5a1b07a1b4e1d6f83b5d
Griffin Tang <gltang@andrew.cmu.edu>
2021-01-09 17:52:28 -0800

    oh god

--- trtl.lua
+++ trtl.lua
@@ -81,7 +81,9 @@ function Trtl:goFoward()
 		self.x = self.x - 1
 	else
 		turtle.dig()
+		os.sleep(0.05)
 		self:goFoward()
+		os.sleep(0.05)
 	end
 end
 
@@ -145,6 +147,7 @@ function Trtl:moveToPoint(targx,targy,targz)
 	--find the direction to go and then go
 	--if no condition is met, we are there baby
 	--use elseif so that you solve one at a time and multiple arent triggered
+	print("I'm at: ".. "(X: " .. self.x .. " Y: " .. self.y .. " Z: " .. self.z .. ") Headed to: (Y:"..targx.." Y:"..targy.." Z:"..targz..")")
 	if self.x < targx then     -- need to go East (target is +x)
 		self:turnToDirection('east')
 		self:goFoward()
