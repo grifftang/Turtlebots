0a5ce33d142dca86130c532d0c5393743d25e2fe
Kyle Slager <kwslager@gmail.com>
2021-01-10 01:58:52 -0600

    yes
    no

---
+++ main2.lua
@@ -0,0 +1,14 @@
+require("trtl")
+print("ready for orders baus")
+
+t = trtl:create{}
+--t:setID()
+--print(t.id)
+-- print(t.id)
+-- t:turnRight{}
+-- t:sayDirection{}
+--t:runMiningSequence(3)
+--t:sayTime()
+
+--t:testDirection()
+t:runLumberjackMeOff(10) --L,W,H
