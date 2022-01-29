bf2782fca073ff182e7ba573f84ddf0ea1739b21
Kyle Slager <kwslager@gmail.com>
2022-01-29 14:26:16 -0600

    boobies

--- Constants.lua
+++ Constants.lua
@@ -1,6 +1,11 @@
 --Constants
 --Author: Kyle "Robust" Slager
 
+--Definition of the path preceding what is user created
+function getInitPath()
+	return ""
+end
+
 --START TASK CONSTANTS
 
 --String for read task
@@ -14,6 +19,20 @@ function taskWrite()
 end
 
 --END TASK CONSTANTS
+
+--START SUB TASK CONSTANTS
+
+--String for update subtype of task write
+function writeUpdate()
+	return "update"
+end
+
+--String for add subtype of task write
+function writeAdd()
+	return "add"
+end
+
+--END SUB TASK CONSTANTS
 
 
 --START MESSAGE CONSTANTS
@@ -36,6 +55,11 @@ end
 --String for lines key
 function msgLines()
 	return "lines"
+end
+
+--String for data key
+function msgData()
+	return "data"
 end
 
 --END MESSAGE CONSTANTS
--- DatabaseCode.lua
+++ DatabaseCode.lua
@@ -11,12 +11,13 @@ function onBoot()
 end
 
 --START DELEGATORS
+
 --Code for checking which task has been requested
 function taskDelegation(msgTable)
-	if taskRead(msgTable) then
+	if msgTable[const.msgTask()]==taskRead() then
 		read()
 	end
-	if taskWrite(msgTable) then
+	if msgTable[const.msgTask()]==taskWrite() then
 		write()
 	end
 end
@@ -25,13 +26,15 @@ end
 function adminTaskDelegation(msgTable)
 
 end
+
 --END DELEGATORS
 
 
 --START TASK FUNCTIONS
+
 --Executes the read function and sends a rednet message with the result
 function read(msgTable)
-	fullPath = getInitPath() .. msgTable[const.msgDirectory()] .. msgTable[const.msgRequest()]
+	fullPath = const.getInitPath() .. msgTable[const.msgDirectory()] .. msgTable[const.msgRequest()]
 	if not fs.exist(fullPath) then
 		return nil
 	file = fs.open(fullPath, "r")
@@ -47,12 +50,35 @@ end
 
 --Executes the write function to store new information to the "database"
 function write(msgTable)
+	directory = const.getInitPath() .. msgTable[const.msgDirectory()]
+	if not fs.exist(directory) then 
+		fs.makeDir(directory)
+	end
+	filename = directory .. msgTable[const.msgRequest()]
+	if msgTable[const.writeUpdate()] then
+		if fs.exist(filename) then
+			fs.delete(filename)
+		end
+		writeData(msgTable)
+		return
+	end
+	if msgTable[const.writeAdd()]
 
 end
+
 --END TASK FUNCTIONS
 
+--START TASK HELPER FUNCTIONS
 
---Definition of the path preceding what is user created
-function getInitPath()
-	return ""
+--Function to add data to a text file
+function writeData(msgTable)
+	file = fs.open(filename, "w")
+	if msgTable[const.msgLines()]==1 or msgTable[const.msgLines()] == nil then
+		file.writeLine(msgTable[const.msgData()])
+	end
+	for i=1,msgTable[const.msgLines()] do
+		file.writeLine(msgTable[const.msgData()][i])
+	end
 end
+
+--END TASK HELPER FUNCTIONS
